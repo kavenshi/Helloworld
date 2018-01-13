61965093aa71b18407ffbb8e1f27143e7a4dd9d5
kavenshi <shilinpeng1990@aliyun.com>
2018-01-13 11:31:44 +0800

    2018.1.12
    upgrad

--- weixinpython.py
+++ weixinpython.py
@@ -1,16 +1,5 @@
 # -*-coding:utf-8-*-
 import urllib2
-import time
-import urllib.error
 
 url = 'http://weixin.sogou.com/weixin?type=2&query=物联网&ie=utf8&s_from=input&_sug_=y&_sug_type_='
-
-headers=[];
-
-urllib.install_opener(opener)
-listurl=[]
-
-def use_proxy(proxy_addr,url)
-    try:
-        import url.request
-        proxy_addr
+nex = 'http://weixin.sogou.com/weixin?query=物联网&_sug_type_=&s_from=input&_sug_=y&type=2&page=2&ie=utf8'
